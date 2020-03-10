cd83a2c013f9e2be87960723fb8c4a942be39be5
Pedro <pedro.mipa@hotmail.com>
2020-03-10 14:16:11 -0300

    up

---
+++ Exs Revisao/ex02.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void converteHora(int totalMinutos, int *hora, int *minutos){
+
+  if (totalMinutos>=60){
+    *hora = totalMinutos/60;
+    *minutos = totalMinutos%60;
+  }else{
+    *hora = 0;
+    *minutos = totalMinutos;
+  }
+}
+
+int main(){
+  int totalMinutos;
+  int hora, minutos;
+
+  printf("Digite os minutos: \n");
+  scanf("%d", &totalMinutos);
+
+  converteHora(totalMinutos, &hora, &minutos);
+
+  printf("%d horas e %d minutos\n", hora, minutos);
+  return 0;
+}
