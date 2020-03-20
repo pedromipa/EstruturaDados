27add4a4d82be67a9b18ba5ff424f29185b76fa4
pedromipa <pedro.mipa@hotmail.com>
2020-03-20 20:14:02 -0300

    att pilha dinamica

--- Aulas/Pilhas/pilhaDinamica.c
+++ Aulas/Pilhas/pilhaDinamica.c
@@ -1,4 +1,5 @@
 #include "pilhaDinamica.h"
+#include <time.h>
 
 void iniciaPilha(PilhaDinamica *p) {
   p->topo = NULL;
@@ -14,10 +15,47 @@ int tamanhoPilha(PilhaDinamica *p) {
 }
 
 void empilha(Objeto obj, PilhaDinamica *p) {
-  PONT aux = (PONT) malloc(sizeof(obj));
+  PtrNoPilha aux;
+  aux = (PtrNoPilha) malloc(sizeof(NoPilha));
+  aux->obj = obj;
+  aux->proximo = p->topo;
+  p->topo = aux;
+  p->tamanho ++;
+}
+
+void desempilha(PilhaDinamica *p, Objeto *obj){
+  if(estaVazia(p)){
+    printf("A pilha ja esta vazia");
+  }
+  PtrNoPilha aux;
+  aux = p->topo;
+  p->topo = aux->proximo;
+  free(aux);
+  p->tamanho --;
+}
+
+void imprimePilha(PilhaDinamica *p){
+
+}
+
+void topo(PilhaDinamica *p, Objeto *obj){
 
 }
 
 int main(){
+  PilhaDinamica pilha;
+  Objeto obj;
+  srand(time(NULL));
+
+  iniciaPilha(&pilha);
+
+  for(int i=0; i<10; i++){
+    obj.chave = rand()%100;
+    empilha(obj, &pilha);
+  }
+   imprimePilha(&pilha);
+
+   desempilha(&pilha, &obj);
+
   return 0;
 }
