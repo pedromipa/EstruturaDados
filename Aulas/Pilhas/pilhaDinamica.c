1048504a9b9b3f9ea47fba176953fcc2da3e4da4
pedromipa <pedro.mipa@hotmail.com>
2020-03-20 11:10:35 -0300

    Inicio  pilha dinamica.

--- Aulas/Pilhas/pilhaDinamica.c
+++ Aulas/Pilhas/pilhaDinamica.c
@@ -1,1 +1,23 @@
 #include "pilhaDinamica.h"
+
+void iniciaPilha(PilhaDinamica *p) {
+  p->topo = NULL;
+  p->tamanho = 0;
+}
+
+bool estaVazia(PilhaDinamica *p) {
+  return (p->tamanho == 0);
+}
+
+int tamanhoPilha(PilhaDinamica *p) {
+  return (p->tamanho);
+}
+
+void empilha(Objeto obj, PilhaDinamica *p) {
+  PONT aux = (PONT) malloc(sizeof(obj));
+
+}
+
+int main(){
+  return 0;
+}
