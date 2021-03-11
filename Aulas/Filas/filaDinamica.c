9b76e8a2a3ad92056c6a96aaf4ea72ad86e8f32b
pedromipa <55238366+pedromipa@users.noreply.github.com>
2021-03-11 11:30:59 -0300

    file
    update file filaDinamica

---
+++ Aulas/Filas/filaDinamica.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+  int chave;
+  /* outros componentes */
+} Objeto;
+
+typedef struct NoFila *PtrNoFila;
+
+typedef struct NoFila{
+  Objeto obj;
+  PtrNoFila proximo;
+} NoFila;
+
+typedef struct {
+  PtrNoFila inicio;
+  PtrNoFila fim;
+  int tamanho;
+} FilaDinamica;
+
+void iniciaFila(FilaDinamica *fila);
+void imprimeFila(FilaDinamica *fila);
+void enfileira(Objeto x, FilaDinamica *fila);
+void desenfileira(FilaDinamica *fila, Objeto *x);
+//Objeto desenfileira(FilaDinamica *fila);
+int estaVazia(FilaDinamica *fila);
+int tamanhoFila(FilaDinamica *fila);
+Objeto inicioFila(FilaDinamica *fila);
+Objeto fimFila(FilaDinamica *fila);
