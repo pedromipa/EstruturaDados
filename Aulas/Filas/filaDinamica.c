dd6fa4a19bd54842ff15f2aba7114f08f5658dcc
pedromipa <55238366+pedromipa@users.noreply.github.com>
2021-03-11 18:31:57 -0300

    updates filadinamica
    implemetação fila dinamica

--- Aulas/Filas/filaDinamica.c
+++ Aulas/Filas/filaDinamica.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 typedef struct {
   int chave;
-  /* outros componentes */
 } Objeto;
 
 typedef struct NoFila *PtrNoFila;
 
 typedef struct NoFila{
+  int x;
   Objeto obj;
   PtrNoFila proximo;
 } NoFila;
@@ -19,12 +20,107 @@ typedef struct {
   int tamanho;
 } FilaDinamica;
 
-void iniciaFila(FilaDinamica *fila);
-void imprimeFila(FilaDinamica *fila);
-void enfileira(Objeto x, FilaDinamica *fila);
-void desenfileira(FilaDinamica *fila, Objeto *x);
-//Objeto desenfileira(FilaDinamica *fila);
-int estaVazia(FilaDinamica *fila);
-int tamanhoFila(FilaDinamica *fila);
-Objeto inicioFila(FilaDinamica *fila);
-Objeto fimFila(FilaDinamica *fila);
+// Iniciar a fila
+void iniciaFila(FilaDinamica *fila){
+  fila->inicio = NULL;
+  fila->fim = NULL;
+  fila->tamanho = 0;
+
+}//void iniciaFila
+
+int tamanhoFila(FilaDinamica *fila){
+  return(fila->tamanho);
+}//tamanhoFila
+
+bool estaVazia(FilaDinamica *fila){
+  return(fila->tamanho == 0);
+}//estaVazia
+
+// Inserção de elementos na fila
+void enfileira(FilaDinamica *fila, int x){
+
+  PtrNoFila aux;
+  aux = (PtrNoFila)malloc(sizeof(NoFila));
+  aux->x = x;
+
+  if (estaVazia(fila)){
+    fila->inicio = aux;
+    fila->fim = aux;
+    aux->proximo = NULL;
+
+  }else{
+    aux->proximo = NULL;
+    fila->fim->proximo=aux;
+    fila->fim = aux;
+  }
+
+  fila->tamanho++;
+}//enfileira
+
+void imprimeFila(FilaDinamica *fila){
+  printf("Fila = { ");
+  PtrNoFila aux;
+  int n = fila->tamanho;
+
+  for (aux = fila->inicio; aux!=NULL; aux = aux->proximo) {
+    printf("%d ", aux->x);
+  }
+
+  printf("}\n");
+}//imprimeFila
+
+int desenfileira(FilaDinamica *fila){
+  int temp = -999;
+  PtrNoFila aux;
+
+  if (!estaVazia(fila)){
+    aux = fila->inicio;
+    fila->inicio = fila->inicio->proximo;
+    temp= aux->x;
+    free(aux);
+    fila->tamanho--;
+
+  }else{
+    printf("A fila esta vazia\n");
+  }
+
+  return(temp);
+}//desenfileira
+
+// int inicioFila(FilaDinamica *fila){
+//   return(fila->array[fila->inicio].chave);
+// }//inicioFila
+
+
+// int fimFila(FilaDinamica *fila){
+//   return(fila->array[fila->fim].chave);
+//
+// }//fimFila
+
+int main(){
+  FilaDinamica fila;
+  Objeto x;
+  int y;
+
+  iniciaFila(&fila);
+    imprimeFila(&fila);
+
+  enfileira(&fila, 10);
+  enfileira(&fila, 230);
+  enfileira(&fila, 123);
+  enfileira(&fila, 1);
+  enfileira(&fila, 5);
+
+  imprimeFila(&fila);
+
+  y = desenfileira(&fila);
+    imprimeFila(&fila);
+
+  enfileira(&fila, 5);
+    imprimeFila(&fila);
+
+  //  printf("Objeto no inicio: %d\n", inicioFila(&fila));
+    //printf("Objeto no fim: %d\n", fimFila(&fila));
+
+  return 0;
+}
