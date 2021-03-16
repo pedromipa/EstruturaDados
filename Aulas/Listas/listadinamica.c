7dd565dbf24fe3c9560375f156936448315458dd
pedromipa <55238366+pedromipa@users.noreply.github.com>
2021-03-16 14:06:25 -0300

    listas
    starting lists

---
+++ Aulas/Listas/listadinamica.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct  {
+  int chave;
+} Objeto;
+
+typedef struct NoLista *PtrNoLista;
+
+typedef struct NoLista {
+    Item elemento;
+    PtrNoLista proximo;
+} NoLista;
+
+typedef struct {
+    PtrNoLista primeiro;
+    int tamanho;
+} Lista;
+
+void iniciaLista(Lista *list){
+
+}
+
+void inserir(Lista *list, Objeto x){
+
+}
+
+void imprimirLista(Lista *list){
+
+}
+
+void destruirLista(Lista *list){
+
+}
+
+bool estaVazia(Lista *list){
+
+}
+
+bool pesquisar(Lista *list, int key){
+
+}
+
+int tamanhoLista(Lista *list){
+
+}
+
+void removeElemento(Lista *list, int chave, Objeto *item){
+
+}
+
+void removePrimeiro(Lista *list, Objeto *item){
+
+}
+
+void removeUltimo(Lista *list, Objeto *item){
+
+}
+
+Objeto primeiro(Lista *list){
+
+}
+
+Objeto ultimo(Lista *list){
+
+}
+
+int main() {
+
+  return 0;
+}
