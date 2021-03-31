5afd7c7b216596897eb26b74ac0749d31c25985f
pedromipa <55238366+pedromipa@users.noreply.github.com>
2021-03-31 11:48:20 -0300

    update lista
    implementação lista single-linkage

--- Aulas/Listas/listadinamica.c
+++ Aulas/Listas/listadinamica.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-// test
-typedef struct  {
-  int chave;
-} Objeto;
 
-typedef struct NoLista *PtrNoLista;
+//Lista Single-linkage
+
+typedef struct NoLista* PtrNoLista;
 
 typedef struct NoLista {
     int chave;
@@ -21,11 +19,10 @@ typedef struct {
 void iniciaLista(Lista *lista){
   lista->primeiro = NULL;
   lista->tamanho = 0;
-
 }
 
 bool estaVazia(Lista *lista){
-    return(lista->tamanho=0);
+    return(lista->tamanho==0);
 }
 
 int tamanhoLista(Lista *lista){
@@ -33,28 +30,32 @@ int tamanhoLista(Lista *lista){
 }
 
 void inserir(Lista *lista, int x){
-  if(estaVazia(lista)){
+  
+    //criaçao e alocação do ponteiro novo
     PtrNoLista novo;
-    //
     novo = (PtrNoLista)malloc(sizeof( NoLista));
-    //
     novo->chave = x;
-    //
-    novo->proximo = NULL;
-    //
-    lista->primeiro = novo;
-    //
 
+  //situação 1: se a lista vazia ou
+  //o elemento que queremos inserir é menor que o primeiro
+  if(estaVazia(lista) || x < lista->primeiro->chave){
+    novo->proximo = lista->primeiro;
+    lista->primeiro = novo;
+
+  //situação 2: aux percorre a lista para encontrar o ponto de inserção
   }else{
     PtrNoLista aux;
-
-    aux = lista->primeiro
-    while (aux->proximo!=NULL && elemento > aux->proximo->chave) {
+    aux = lista->primeiro;
+    while (aux->proximo!=NULL && x > aux->proximo->chave) {
       aux = aux->proximo;
-    }
+    }//while
+
+    novo->proximo = aux->proximo;
+    aux->proximo = novo;
+
   }//if else
 
-  lista->tamanho = lista->tamanho + 1;
+  lista->tamanho ++;
 
 }//void inserir
 
@@ -69,40 +70,65 @@ void imprimirLista(Lista *lista){
   printf("}\n");
 
 }
+//
+// void destruirLista(Lista *lista){
+//
+// }
+//
+//
+// bool pesquisar(Lista *lista, int key){
+//
+// }
 
-void destruirLista(Lista *lista){
-
-}
-
-
-bool pesquisar(Lista *lista, int key){
-
-}
-
-
-void removeElemento(Lista *lista, int chave, Objeto *item){
-
-}
-
-void removePrimeiro(Lista *lista, Objeto *item){
-
-}
-
-void removeUltimo(Lista *lista, Objeto *item){
-
-}
-
-Objeto primeiro(Lista *lista){
-
-}
-
-Objeto ultimo(Lista *lista){
-
-}
+//
+// void removeElemento(Lista *lista, int chave, Objeto *item){
+//
+// }
+//
+// void removePrimeiro(Lista *lista, Objeto *item){
+//
+// }
+//
+// void removeUltimo(Lista *lista, Objeto *item){
+//
+// }
+//
+// Objeto primeiro(Lista *lista){
+//
+// }
+//
+// Objeto ultimo(Lista *lista){
+//
+// }
 
 int main() {
   Lista lista;
   iniciaLista(&lista);
+
+  inserir(&lista, 555);
+  inserir(&lista, 0);
+  inserir(&lista, 20);
+  inserir(&lista, 7000);
+
+
+  imprimirLista(&lista);
+  printf("Tamanho = %d\n", tamanhoLista(&lista));
+
+
+
+  // //testar elemento que existe
+  // if(pesquisaListaDinamica2(&listinha, 13)) {
+  //   printf("Achou miseravi \n");
+  // } else {
+  //   printf("Try again\n");
+  // }
+  //
+  // if(pesquisaListaDinamica2(&listinha, 87)) {
+  //   printf("Achou miseravi \n");
+  // } else {
+  //   printf("Try again \n");
+  // }
+  //
 
   return 0;
 }
